562602813f140da7c258d4bb5748a7640a50fa9b
illefeliz72 <elifelize.herreria@lorma.edu>
2026-01-16 03:35:49 +0800

    class object

---
+++ Laptop.java
@@ -0,0 +1,9 @@
+public class Laptop{
+    String brand;
+    double storage;
+    double memory;
+    double screen;
+    String model;
+    String condition;
+    double price;
+}
---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        Laptop obj1 = new Laptop();
+        obj1.brand = "HP";
+        obj1.storage = 512;
+        obj1.screen = 12.5;
+        obj1.model = "Probook 4540s";
+        obj1.condition = "old";
+        obj1.price = 35000;
+
+        Laptop obj2 = new Laptop();
+        obj2.brand = "Asus";
+        obj2.storage = 256;
+        obj2.screen = 12;
+        obj2.model = "gyat";
+        obj2.condition = "new"
+        obj2.price = 67000;
+    }
+}
