28d4c1cb6833358f51a422a27255079d1d2ca429
illefeliz72 <elifelize.herreria@lorma.edu>
2026-01-30 04:00:37 +0800

    activity2oop

--- activity2/Course.java
+++ activity2/Course.java
@@ -5,18 +5,37 @@ public class Course {
     String courseTitle;
     Student[] enrolledStudents;
     int enrollmentCount = 0;
-    String schoolName = "My University";
+    static String schoolName = "My University";
 
     public Course() {
     }
 
     public Course(String courseCode, String courseTitle) {
-    this.enrolledStudents = new Student[50];
+        this.enrolledStudents = new Student[50];
+        this.courseCode = courseCode;
+        this.courseTitle = courseTitle;
     }
 
     public void enrollStudent(Student student) {
-        if (enrolledStudents) {
-            
+        if (enrollmentCount < enrolledStudents.length) {
+            enrolledStudents[enrollmentCount] = student;
+            enrollmentCount++;
         }
+    }
+
+    public void displayCourseInfo() {
+        System.out.printf("""
+                School: %s
+                Course:  %s %s
+                """,schoolName,courseCode,courseTitle);
+        System.out.println("Enrolled Students: ");
+        for(int i = 0; i < enrollmentCount; i++){
+            System.out.println("- Student " + (i + 1));
+            enrolledStudents[i].displayStudentInfo();
+        }
+
+    }
+    public static String getSchoolName(){
+        return schoolName;
     }
 }
