e320a35354eb93e085351dc88862c977b51eb29f
illefeliz72 <elifelize.herreria@lorma.edu>
2026-03-05 12:03:11 +0800

    act8

---
+++ activity8/MyRectangle2D.java
@@ -0,0 +1,78 @@
+package activity8;
+
+public class MyRectangle2D {
+    private double x;
+    private double y;
+    private double width;
+    private double height;
+
+    public MyRectangle2D() {
+        this(0, 0, 1, 1);
+    }
+
+    //constructor
+    public MyRectangle2D(double x, double y, double width, double height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+
+    }
+
+    // getter
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    // setter
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    // methods
+    public double getArea() {
+        return width * height;
+    }
+
+    public double getPerimeter() {
+        return 2 * (height + width);
+    }
+
+    public boolean contains(double x, double y) {
+        return (x >= x - width / 2 &&
+                x <= x + width / 2 &&
+                y >= y - height / 2 &&
+                y <= y + height / 2);
+
+    }
+    public boolean contains(MyRectangle2D r){
+        return
+    }
+    public boolean overlaps(MyRectangle2D r){
+        
+    }
+}
