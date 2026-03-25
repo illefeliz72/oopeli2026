e6f09e2f5b3e75bcdae2a109488b206616adb303
illefeliz72 <elifelize.herreria@lorma.edu>
2026-03-25 11:17:56 +0800

    .

--- activity10/Main.java
+++ activity10/Main.java
@@ -1,5 +1,31 @@
 package activity10;
 
+import java.util.ArrayList;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        ArrayList<GeometricObject> shapes = new ArrayList<>();
+
+        // 1. Adding objects to the list
+        shapes.add(new Circle(5.0, "Red", true));
+        shapes.add(new Circle(5.0, "Blue", false)); // Same radius as first
+        shapes.add(new Rectangle(4.0, 10.0, "Green", true));
+        shapes.add(new Rectangle(2.0, 5.0, "Yellow", false));
+
+        // 2. Iterating and showing polymorphism
+        System.out.println("--- Shape Details ---");
+        for (GeometricObject shape : shapes) {
+            System.out.println(shape.toString());
+            System.out.printf("Area: %.2f | Perimeter: %.2f%n", shape.getArea(), shape.getPerimeter());
+            System.out.println("----------------------");
+        }
+
+        // 3. Demonstrating equals()
+        System.out.println("\n--- Testing Equality ---");
+        boolean circlesMatch = shapes.get(0).equals(shapes.get(1));
+        System.out.println("Circle 1 equals Circle 2 (Same Radius): " + circlesMatch);
+
+        boolean rectMatch = shapes.get(2).equals(shapes.get(3));
+        System.out.println("Rectangle 1 equals Rectangle 2 (Different Dims): " + rectMatch);
+    }
 }
--- activity10/Rectangle.java
+++ activity10/Rectangle.java
@@ -1,5 +1,49 @@
 package activity10;
 
-public class Rectangle {
-    
+public class Rectangle extends GeometricObject {
+    private double width;
+    private double height;
+
+    public Rectangle() {
+        super();
+    }
+
+    public Rectangle(double width, double height) {
+        super();
+        this.width = width;
+        this.height = height;
+    }
+
+    public Rectangle(double width, double height, String color, boolean filled) {
+        super(color, filled);
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return width * height;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * (width + height);
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle [Width: " + width + ", Height: " + height + ", Color: " + getColor() + ", Filled: "
+                + isFilled() + "]";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof Rectangle))
+            return false;
+        Rectangle other = (Rectangle) o;
+        return Double.compare(this.width, other.width) == 0 &&
+                Double.compare(this.height, other.height) == 0;
+    }
 }
