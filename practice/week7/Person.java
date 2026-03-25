c9622dcfd80c5c18183f76564bca502a22431b73
illefeliz72 <elifelize.herreria@lorma.edu>
2026-03-25 11:19:34 +0800

    .

--- practice/week7/Person.java
+++ practice/week7/Person.java
@@ -1,62 +1,52 @@
 package practice.week7;
 
-import java.time.LocalTime;
-
+import java.time.LocalDate;
 public class Person {
     private String firstName;
     private String lastName;
+    private LocalDate birthDate;
     private String gender;
-    private LocalTime birthDate;
-    private String phone;
-    private String email;
+
+    public Person(){}
+
     
+
+    public Person(String firstName, String lastName, LocalDate birthDate, String gender) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.gender = gender;
+    }
+
+
+
     public String getFirstName() {
         return firstName;
     }
-
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-
     public String getLastName() {
         return lastName;
     }
-
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
     public String getGender() {
         return gender;
     }
-
     public void setGender(String gender) {
         this.gender = gender;
     }
 
-    public LocalTime getBirthDate() {
-        return birthDate;
+    public String getInfo(){
+        return "Person: " + firstName + " " + lastName;
     }
-
-    public void setBirthDate(LocalTime birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-
-    }
+    
+}
--- practice/week7/Student.java
+++ practice/week7/Student.java
@@ -1,15 +1,59 @@
 package practice.week7;
 
+import java.time.LocalDate;
+
 public class Student extends Person {
+    private int studentID;
+    private String course;
+    private int year;
 
-private int year;
-private String course;
-private int studentID;
+    public Student(){}
 
-public Student(int studentID, String course, int year) {
+    public Student(String firstName, String lastName, LocalDate birthDate, String gender, int studentID, String course,
+            int year) {
+            // super.setFirstName(firstName);
+            // super.setLastName(lastName);
+            super(firstName, lastName, birthDate, gender);
+            this.course = course;
+            this.studentID = studentID;
+            this.year = year;
+        
+    }
+    public Student(int studentID, String course, int year) {
         this.studentID = studentID;
         this.course = course;
         this.year = year;
-        //continuing this later guh
     }
+    public int getStudentID() {
+        return studentID;
+    }
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
+    }
+    public String getCourse() {
+        return course;
+    }
+    public void setCourse(String course) {
+        this.course = course;
+    }
+    public int getYear() {
+        return year;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override   //annotation
+    public String getInfo(){
+        return "Student: " + super.getFirstName() + " " + super.getLastName();
+    }
+
+    //Overloading
+    public String getInfo(String format){
+        if(format.equals("U"))
+            return getInfo().toUpperCase();
+
+        return getInfo();
+    }
+    
 }
