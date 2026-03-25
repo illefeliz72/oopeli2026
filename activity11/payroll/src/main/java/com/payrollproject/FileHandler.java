a93e482aed5edc84335a01a77035e6e8a836944e
illefeliz72 <elifelize.herreria@lorma.edu>
2026-03-25 12:28:37 +0800

    .

--- activity11/payroll/src/main/java/com/payrollproject/FileHandler.java
+++ activity11/payroll/src/main/java/com/payrollproject/FileHandler.java
@@ -1,8 +1,25 @@
 package com.payrollproject;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.typeadapters.RuntimeTypeAdapterFactory;
+import java.io.*;
 
 public class FileHandler {
     private static final String FILE_NAME = "employees.json";
     private final Gson gson;
+
+    public FileHandler() {
+      
+        RuntimeTypeAdapterFactory<Employee> adapter = RuntimeTypeAdapterFactory
+            .of(Employee.class, "type")
+            .registerSubtype(SalariedEmployee.class, EmployeeType.SALARIED.name())
+            .registerSubtype(HourlyEmployee.class, EmployeeType.HOURLY.name());
+        this.gson = new GsonBuilder()
+                .registerTypeAdapterFactory(adapter)
+                .setPrettyPrinting()
+                .create();
+        Gson gson = new GsonBuilder().registerTypeAdapterFactory(adapter).create();
+    }
+    
 }
